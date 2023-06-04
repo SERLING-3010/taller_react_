fcb40861985d29305d0f2cb4663c1cbc8baf32ed
sergio rodriguez <sergiolra637796@gmail.com>
2023-06-04 00:31:41 -0500

    sexto commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import HolaMundo from './components/HolaMundo.jsx'
-import Variables from './components/variables.jsx'
-import Contador from './components/Contador.jsx'
+
 import PeticionApi from './components/PeticionApi.jsx'
 
 
